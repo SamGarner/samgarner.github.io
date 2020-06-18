d08ce70f765a8fb68ae18417c6e48d8831fd57bc
SamGarner <samuel.w.garner@gmail.com>
2020-06-17 19:57:13 -0500

    Finish JS gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,28 @@
 function activateGallery() {
-	alert("Hello from the gallery file!");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	//let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+	let mainImage = document.querySelector("#gallery-photo > img");
+
+
+	thumbnails.forEach(function(thumbnail) {
+		thumbnail.addEventListener("click", function() {
+			// set clicked image as main image
+			mainImage.setAttribute("src", thumbnail.dataset.largeVersion);
+			mainImage.setAttribute("alt", thumbnail.alt);
+
+			//change which image is current.
+			document.querySelector(".current").classList.remove("current");
+			thumbnail.parentNode.classList.add("current");
+
+			// Update image info
+			let galleryInfo = document.querySelector("#gallery-info");
+			let title 			= galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
+		
+			title.innerHTML			  = thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
+		});
+	});
 }
+
+
